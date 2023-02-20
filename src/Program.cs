273f48ab19c8fc7b6ec3bd635fc5559a21d7d311
osawa-koki <github@osawa.dev>
2023-02-21 04:02:20 +0900

    完成した。

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,13 @@
+using System.Text.Json;
+using dotenv.net;
+
+// .envファイルの読み込み
+DotEnv.Load(options: new DotEnvOptions(trimValues: true));
+var envVars = DotEnv.Read();
+var scheme = envVars["SCHEME"];
+var host = envVars["HOST"];
+var port = envVars["PORT"];
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,11 +24,37 @@ if (app.Environment.IsDevelopment())
   app.UseSwaggerUI();
 }
 
-app.MapGet("/:num", () =>
+app.MapGet("/{num:int}", (int num) =>
 {
+  if (num < 1)
+  {
+    return Results.BadRequest(new
+    {
+      error = "Number must be greater than 0.",
+    });
+  }
+
+  if (num == 1)
+  {
+    return Results.Ok(new
+    {
+      num = 1,
+    });
+  }
+
+  // HTTPリクエストを送信する
+  var client = new HttpClient();
+  var response = client.GetAsync($"{scheme}://{host}:{port}/{num - 1}").Result;
+  var content = response.Content.ReadAsStringAsync().Result;
+  var jsonDoc = JsonDocument.Parse(content);
+  var n = jsonDoc.RootElement.GetProperty("num").GetInt32();
+
+  return Results.BadRequest(new {
+    num = num * n,
+  });
 
 })
 .WithName("Factorial")
 .WithOpenApi();
 
-app.Run();
+app.Run($"http://+:{port}");
